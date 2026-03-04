cf27c79c6f63797a53730102f35c35f750f0e119
Jefferson Nalvarte <jefroy95@gmail.com>
2026-03-04 18:46:50 -0500

    Cadena nueva

--- FISIOKIDS/probando/Program.cs
+++ FISIOKIDS/probando/Program.cs
@@ -2,3 +2,5 @@
 Console.WriteLine("Hello, Estudiante de platzi!!!");
 
 string stripe_api_key = "sk_test_51N8mLhHj1n9sQG7Xo5q2Zt3v1z25q2v1z2q2v1z2q2v1z2q";
+
+string testingConnection = "Server=tcp:localhost,1433;Initial Catalog=FisioKids;Persist Security Info=False;User ID=sa;Password=your_password;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
