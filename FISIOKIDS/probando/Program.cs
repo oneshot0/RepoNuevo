91cd758df18cb6cb91a6eebce9af07bb0d27881a
Jefferson Nalvarte <jefroy95@gmail.com>
2026-03-04 18:35:58 -0500

    Nuevos cambios

--- FISIOKIDS/probando/Program.cs
+++ FISIOKIDS/probando/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Estudiante de platzi!!!");
+
+string stripe_api_key = "sk_test_51N8mLhHj1n9sQG7Xo5q2Zt3v1z25q2v1z2q2v1z2q2v1z2q";
